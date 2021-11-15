818caa7632d6fcc161de515fdfabf245074b56d3
cjh8746 <cjh87467@gmail.com>
2021-11-15 15:23:21 +0900

    test : 테스트 코드 추가

--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -3,20 +3,35 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import javax.swing.text.Position;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-public class PositionTest {
+class PositionTest {
     private static final int NEGATIVE_NUMBER = -1;
     private Position position;
+
+    @Test
+    @DisplayName("객체 생성시 입력받은 양수를 position 으로 초기화 한다.")
+    void createPosition()
+    {
+        //given
+        int inputNumber = 3;
+        int expectedNumber = 3;
+
+        //when
+        Position actual = new Position(inputNumber);
+
+        //then
+        assertThat(actual).isEqualTo(new Position(expectedNumber));
+    }
 
     @Test
     @DisplayName("포지션에 대한 값이 음수일경우 IllegalArgumentException 반환 .")
     void validateName() {
         //then
-        assertThatThrownBy(() -> {
-            new Position(NEGATIVE_NUMBER);
-        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new model.Position(NEGATIVE_NUMBER))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("position 값이 음수입니다.");
     }
 }
