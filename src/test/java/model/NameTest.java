f6b7369a7f33fcbcc28676e2bec1c723581cb39f
cjh8746 <cjh87467@gmail.com>
2021-11-15 15:50:10 +0900

    test : 테스트 코드 수정

--- src/test/java/model/NameTest.java
+++ src/test/java/model/NameTest.java
@@ -3,11 +3,10 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatIllegalStateException;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
-    private Name name;
-
     @Test
     @DisplayName("이름 객체 생성시, 입력받은 문자열을 이름으로 저장한다.")
     void createName() {
@@ -19,7 +18,7 @@ class NameTest {
         Name actual = new Name(inputName);
 
         //then
-        assertThat(actual).isEqulsTo(new Name(expectedName));
+        assertThat(actual).isEqualTo(new Name(expectedName));
     }
 
     @DisplayName("이름 객체의 이름의 길이는  5자를 초과할 수 없다.")
@@ -28,8 +27,8 @@ class NameTest {
         String inputName = "JiHwan2";
 
         //then
-        assertThatIllegalStateException(() -> new Name(inputName))
-                .isThrownBy(IllegalArgumentException.class)
-                .withMessage("Name 값의 길이는 5를 초과할 수 없습니다.");
+        assertThatThrownBy(() -> new Name(inputName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Name 값의 길이는 5를 초과할 수 없습니다.");
     }
 }
