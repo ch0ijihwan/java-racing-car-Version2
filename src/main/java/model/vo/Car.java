e93792ad79ade78505c4db25db4f5362be428aa4
cjh8746 <cjh87467@gmail.com>
2021-11-19 13:40:06 +0900

    refactor: 생성자 변경 및 변수명 수정

--- src/main/java/model/vo/Car.java
+++ src/main/java/model/vo/Car.java
@@ -9,8 +9,7 @@ public class Car {
     private final Name name;
 
     public Car(final String name) {
-        this.name = new Name(name);
-        this.position = new Position(START_POSITION);
+        this(name, START_POSITION);
     }
 
     public Car(final String name, final int position) {
@@ -30,8 +29,8 @@ public class Car {
         return this.position.equals(car.position);
     }
 
-    public void move(final boolean isCarMove) {
-        if (isCarMove) {
+    public void move(final boolean movable) {
+        if (movable) {
             this.position = this.position.move(ONE_STEP);
         }
     }
