9856e07162c78bd25ca6fbbcd2743b5456b5572c
cjh8746 <cjh87467@gmail.com>
2022-04-18 15:10:18 +0900

    test : Name 테스트 코드 수정

--- src/test/java/model/vo/NameTest.java
+++ src/test/java/model/vo/NameTest.java
@@ -10,6 +10,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
+    private static final int MAX_NAME_SIZE = 5;
+
     @Test
     @DisplayName("getValue() 호출 시, 이름을 반환한다.")
     void getValue() {
@@ -31,6 +33,6 @@ class NameTest {
     void validateSizeOfName(final String inputtedName) {
         //then
         assertThatThrownBy(() -> new Name(inputtedName)).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("이름의 길이는 5를 초과할 수 없고 공백이 아니어야 합니다.");
+                .hasMessage(String.format("이름의 길이는 %d를 초과할 수 없고 공백이 아니어야 합니다.", MAX_NAME_SIZE));
     }
 }
