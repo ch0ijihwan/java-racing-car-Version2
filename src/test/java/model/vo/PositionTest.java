7cf70003a1dd0a7d5ca5ce560d216ebb674c7c1f
cjh8746 <cjh87467@gmail.com>
2022-04-18 14:15:35 +0900

    test : Position 테스트 코드 작성

--- src/test/java/model/vo/PositionTest.java
+++ src/test/java/model/vo/PositionTest.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class PositionTest {
     @Test
-    @DisplayName("getValue() 호출 시, value 를 반환한다. ")
+    @DisplayName("getValue() 호출 시, value 를 반환한다.")
     void getValue() {
         //given
         final int inputtedValue = 1;
