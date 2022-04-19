383c2e77394d6599e9f20c0adda028f88ef0b93d
cjh8746 <cjh87467@gmail.com>
2022-04-19 15:57:25 +0900

    feat : RacingGame 구현

---
+++ src/main/java/model/RacingGame.java
@@ -0,0 +1,49 @@
+package model;
+
+import model.movement.MovementStrategy;
+import model.vo.Car;
+import model.vo.Cars;
+
+import java.util.List;
+import java.util.Objects;
+
+public class RacingGame {
+    private final Cars racingCars;
+    private final NumberOfAttempt numberOfAttempt;
+
+    public RacingGame(final String[] inputtedNames, final int inputtedNumberOfAttempt) {
+        this.racingCars = new Cars(inputtedNames);
+        this.numberOfAttempt = new NumberOfAttempt(inputtedNumberOfAttempt);
+    }
+
+    public RacingGame(final Cars inputtedCars, final int numberOfAttempt) {
+        this.racingCars = inputtedCars;
+        this.numberOfAttempt = new NumberOfAttempt(numberOfAttempt);
+    }
+
+    public List<Car> getWinners() {
+        return racingCars.getWinners();
+    }
+
+    public void playOneRound(final MovementStrategy movementStrategy) {
+        racingCars.moveAllCar(movementStrategy);
+        numberOfAttempt.decrease();
+    }
+
+    public boolean isGameEnd() {
+        return numberOfAttempt.isEnd();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RacingGame that = (RacingGame) o;
+        return Objects.equals(racingCars, that.racingCars) && Objects.equals(numberOfAttempt, that.numberOfAttempt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(racingCars, numberOfAttempt);
+    }
+}
