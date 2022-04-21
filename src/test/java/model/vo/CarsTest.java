d3a34aba01e87f7ae341e1d78c2e70f0848cf8f8
cjh8746 <cjh87467@gmail.com>
2022-04-21 13:53:42 +0900

    test : 테스트 코드 수정

--- src/test/java/model/vo/CarsTest.java
+++ src/test/java/model/vo/CarsTest.java
@@ -1,5 +1,6 @@
 package model.vo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
+    private Cars cars;
+
+    @BeforeEach
+    void setUp() {
+        cars = new Cars(List.of(
+                Car.of("hi", 3), Car.of("hello", 3),
+                Car.of("happy", 1)
+        ));
+    }
+
     @Test
     @DisplayName("Cars 객체 생성 시, 입력받은 자동차 이름 중 중복이 있으면 예외를 반환한다.")
     void createCars() {
@@ -22,11 +33,24 @@ class CarsTest {
     }
 
     @Test
+    @DisplayName("getCars() 호출 시, cars 를 반환한다.")
+    void getCars() {
+        //given
+        List<Car> expect = List.of(
+                Car.of("hi", 3), Car.of("hello", 3),
+                Car.of("happy", 1));
+
+        //when
+        List<Car> actual = cars.getCars();
+
+        //then
+        assertThat(actual).isEqualTo(expect);
+    }
+
+    @Test
     @DisplayName("getWinners() 호출 시, 자동차들 중 우승자의 이름을 뽑아 리스트 형태로 반환한다. 이때 우승자가 둘 이상일 경우 모두 다 반환한다.")
     void getWinners() {
         //given
-        List<Car> inputtedCars = List.of(Car.of("hi", 3), Car.of("hello", 3), Car.of("happy", 1));
-        Cars cars = new Cars(inputtedCars);
         List<Car> expectedWinners = List.of(Car.of("hello", 3), Car.of("hi", 3));
 
         //when
