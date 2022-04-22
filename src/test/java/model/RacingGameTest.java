ba6f19239a4d9dd86f674483825cfce2d4232c76
cjh8746 <cjh87467@gmail.com>
2022-04-22 11:46:42 +0900

    test : 테스트 코드 수정 null 처리에 대한 테스트 코드 추가 및 코드 정리

--- src/test/java/model/RacingGameTest.java
+++ src/test/java/model/RacingGameTest.java
@@ -3,6 +3,7 @@ package model;
 import model.movement.MovementStrategy;
 import model.vo.Car;
 import model.vo.Cars;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -13,11 +14,26 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class RacingGameTest {
     private RacingGame racingGame;
+    private Cars cars;
+    private String[] names;
     private final MovementStrategy testMovementStrategy = new alwaysMove();
+
+    @BeforeEach
+    void setUp() {
+        names = new String[]{"apple", "hi", "hello"};
+        cars = new Cars(
+                List.of(
+                        Car.of("hello", 1),
+                        Car.of("hi", 2),
+                        Car.of("apple", 2)
+                )
+        );
+    }
 
     static class alwaysMove implements MovementStrategy {
         @Override
@@ -26,19 +42,30 @@ class RacingGameTest {
         }
     }
 
-
     @Test
     @DisplayName("객체 생성 시, 파라미터로 부터 입력받은 자동차 이름들과 시도 횟수를 기반으로 , Cars 객체와 NumberOfAttempt 객체를 만든다.")
     void create() {
         //given
-        String[] inputtedNames = new String[]{"apple", "hi", "hello"};
         int inputtedNumberOfAttempt = 5;
 
         //when
-        racingGame = new RacingGame(inputtedNames, inputtedNumberOfAttempt);
+        racingGame = new RacingGame(names, inputtedNumberOfAttempt);
 
         //then
-        assertThat(racingGame).isEqualTo(new RacingGame(inputtedNames, inputtedNumberOfAttempt));
+        assertThat(racingGame).isEqualTo(new RacingGame(names, inputtedNumberOfAttempt));
+    }
+
+    @Test
+    @DisplayName("생체 생성 시, String [] 타입을 받는 경우 입력 받은 이름들이 null 이면 예외처리를 반환한다.")
+    void validateNull() {
+        //given
+        String[] inputtedNames = null;
+        int inputtedNumberOfAttempt = 2;
+
+        //when
+        assertThatThrownBy(() -> new RacingGame(inputtedNames, inputtedNumberOfAttempt))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("null 값이 입력되었습니다.");
     }
 
     @Test
@@ -46,11 +73,14 @@ class RacingGameTest {
             " 그리고 numberOfAttempt 를 1 감소 시킨다.")
     void playOneRound() {
         //given
-        String[] inputtedNames = new String[]{"apple", "hi"};
         int inputtedNumberOfAttempt = 2;
-        Cars expectedCars = new Cars(List.of(Car.of("apple", 1), Car.of("hi", 1)));
+        Cars expectedCars = new Cars(
+                List.of(Car.of("apple", 1),
+                        Car.of("hi", 1),
+                        Car.of("hello", 1))
+        );
         NumberOfAttempt expectedNumberOfAttempt = new NumberOfAttempt(1);
-        racingGame = new RacingGame(inputtedNames, inputtedNumberOfAttempt);
+        racingGame = new RacingGame(names, inputtedNumberOfAttempt);
 
         //when
         racingGame.playOneRound(testMovementStrategy);
@@ -65,14 +95,12 @@ class RacingGameTest {
         );
     }
 
-
     @ParameterizedTest
     @DisplayName("isGameEnd() 호출 시, 게임이 끝났으면 true 를 반환 그렇지 않으면 false 를 반환한다.")
     @MethodSource("createNumberOfAttemptParameterProvider")
     void isGameEnd(int inputtedNumberOfAttempt, boolean expect) {
         //given
-        String[] inputtedNames = new String[]{"apple", "hi"};
-        racingGame = new RacingGame(inputtedNames, inputtedNumberOfAttempt);
+        racingGame = new RacingGame(names, inputtedNumberOfAttempt);
         racingGame.playOneRound(testMovementStrategy);
 
         //when
@@ -93,16 +121,13 @@ class RacingGameTest {
     @DisplayName("getCarsDuringRacing() 호출 시, 게임 내 자동차들의 리스트를 반환한다.")
     void getCars() {
         //given
-        Cars inputtedCars = new Cars(List.of(
-                Car.of("hello", 1),
-                Car.of("hi", 2),
-                Car.of("apple", 2)));
         List<Car> expectedCars = List.of(
                 Car.of("hello", 1),
                 Car.of("hi", 2),
-                Car.of("apple", 2));
+                Car.of("apple", 2)
+        );
         int inputtedNumberOfAttempts = 2;
-        racingGame = new RacingGame(inputtedCars, inputtedNumberOfAttempts);
+        racingGame = new RacingGame(cars, inputtedNumberOfAttempts);
 
         //when
         List<Car> actual = racingGame.getCarsDuringRacing();
@@ -115,15 +140,12 @@ class RacingGameTest {
     @DisplayName("getWinners() 호출 시, 우승자로 구성된 Car 리스트를 반환한다.")
     void getWinners() {
         //given
-        Cars inputtedCars = new Cars(List.of(
-                Car.of("hello", 1),
-                Car.of("hi", 2),
-                Car.of("apple", 2)));
         int inputtedNumberOfAttempt = 1;
         List<Car> expectedWinners = List.of(
                 Car.of("hi", 2),
-                Car.of("apple", 2));
-        racingGame = new RacingGame(inputtedCars, inputtedNumberOfAttempt);
+                Car.of("apple", 2)
+        );
+        racingGame = new RacingGame(cars, inputtedNumberOfAttempt);
 
         //when
         List<Car> actualWinners = racingGame.getWinners();
