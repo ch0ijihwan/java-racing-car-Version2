4aba0ef4c3496f31d42fd483077aec9bc733864f
cjh8746 <cjh87467@gmail.com>
2022-04-26 15:03:28 +0900

    test : 테스트 코드 수정

--- src/test/java/controller/dto/WinnersDtoTest.java
+++ src/test/java/controller/dto/WinnersDtoTest.java
@@ -1,6 +1,5 @@
 package controller.dto;
 
-import model.vo.Car;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -13,14 +12,14 @@ class WinnersDtoTest {
     @DisplayName("getNames() 호출 시, 입력 받았던 자동차들의 이름을 리스트로 반환한다.")
     void getNames() {
         //given
-        List<Car> cars = List.of(Car.of("apple", 1), Car.of("hi", 1));
-        List<String> expectedNames = List.of("apple", "hi");
-        WinnersDto winnersDto = new WinnersDto(cars);
+        List<String> winnerNames = List.of("apple", "hi");
+        List<String> expect = List.of("apple", "hi");
+        WinnersDto winnersDto = new WinnersDto(winnerNames);
 
         //when
         List<String> actual = winnersDto.getNames();
 
         //then
-        assertThat(actual).isEqualTo(expectedNames);
+        assertThat(actual).isEqualTo(expect);
     }
 }
