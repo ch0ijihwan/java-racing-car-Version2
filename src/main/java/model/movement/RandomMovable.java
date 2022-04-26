6f5b793a5ca8c30741d63ed099b8fbbf008d3d12
cjh8746 <cjh87467@gmail.com>
2022-04-26 15:13:48 +0900

    refactor : 움직임 반환 확률 조정

--- src/main/java/model/movement/RandomMovable.java
+++ src/main/java/model/movement/RandomMovable.java
@@ -3,9 +3,16 @@ package model.movement;
 import java.util.Random;
 
 public class RandomMovable implements MovementStrategy {
-    private final Random RAMDOM = new Random();
+    private static final int NOT_MOVING_VALUE = 4;
+    private static final Random RAMDOM = new Random();
+
     @Override
     public boolean generateMovable() {
-        return RAMDOM.nextBoolean();
+        int judgmentValue = RAMDOM.nextInt(10);
+        return canMove(judgmentValue);
+    }
+
+    private boolean canMove(final int boundaryValue) {
+        return NOT_MOVING_VALUE<= boundaryValue;
     }
 }
