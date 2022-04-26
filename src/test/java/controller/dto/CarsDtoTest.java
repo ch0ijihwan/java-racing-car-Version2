43f77aace6997df997db0de479dbb1056929d40d
cjh8746 <cjh87467@gmail.com>
2022-04-26 15:00:04 +0900

    test : 테스트 코드 수정

--- src/test/java/controller/dto/CarsDtoTest.java
+++ src/test/java/controller/dto/CarsDtoTest.java
@@ -1,6 +1,5 @@
 package controller.dto;
 
-import model.vo.Car;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,18 +13,16 @@ class CarsDtoTest {
 
     @BeforeEach
     void setUp() {
-        List<Car> cars = List.of(
-                Car.of("apple", 1),
-                Car.of("hi", 2),
-                Car.of("hello", 3));
-        carsDto = new CarsDto(cars);
+        List<String> carNames = List.of("apple", "hello");
+        List<Integer> carPositions = List.of(1, 2);
+        carsDto = new CarsDto(carNames, carPositions);
     }
 
     @Test
     @DisplayName("getCarNames(), 호출 시 자동차들의 이름을 반환한다.")
     void getCarNames() {
         //given
-        List<String> expectedNames = List.of("apple", "hi", "hello");
+        List<String> expectedNames = List.of("apple", "hello");
 
         //when
         List<String> actual = carsDto.getCarNames();
@@ -38,7 +35,7 @@ class CarsDtoTest {
     @DisplayName("getCarNames(), 호출 시 자동차들의 위치를 반환한다.")
     void getCarPositions() {
         //given
-        List<Integer> expectedPositions = List.of(1, 2, 3);
+        List<Integer> expectedPositions = List.of(1, 2);
 
         //when
         List<Integer> actual = carsDto.getCarPositions();
