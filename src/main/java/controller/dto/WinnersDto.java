d2749d7a2386e4a7ee3ced3d5d3f948f5d7f853d
cjh8746 <cjh87467@gmail.com>
2022-04-26 15:03:52 +0900

    refactor : dto 수정 로직 제거

--- src/main/java/controller/dto/WinnersDto.java
+++ src/main/java/controller/dto/WinnersDto.java
@@ -1,21 +1,12 @@
 package controller.dto;
 
-import model.vo.Car;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class WinnersDto {
     private final List<String> names;
 
-    public WinnersDto(final List<Car> cars) {
-        this.names = extractCarNames(cars);
-    }
-
-    private List<String> extractCarNames(final List<Car> cars) {
-        return cars.stream()
-                .map(car -> car.getName().getValue())
-                .collect(Collectors.toUnmodifiableList());
+    public WinnersDto(final List<String> winnerNames) {
+        this.names = winnerNames;
     }
 
     public List<String> getNames() {
