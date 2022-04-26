b7b0480e55021457ebedaf679a1026da7856fc38
cjh8746 <cjh87467@gmail.com>
2022-04-26 23:27:02 +0900

    refactor : equals 및 hashCode 재정의

--- src/main/java/model/vo/Names.java
+++ src/main/java/model/vo/Names.java
@@ -1,6 +1,7 @@
 package model.vo;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Names {
@@ -27,6 +28,19 @@ public class Names {
         if (names == null) {
             throw new IllegalArgumentException("null 값이 입력되었습니다.");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Names names1 = (Names) o;
+        return Objects.equals(names, names1.names);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(names);
     }
 
     public List<Name> getNames() {
