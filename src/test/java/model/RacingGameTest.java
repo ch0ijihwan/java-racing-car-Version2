472a7304911994d75f5f78a19e8f6e706ae989e3
cjh8746 <cjh87467@gmail.com>
2022-05-02 16:07:42 +0900

    test : 테스트 코드 수정

--- src/test/java/model/RacingGameTest.java
+++ src/test/java/model/RacingGameTest.java
@@ -1,8 +1,8 @@
 package model;
 
+import model.cars.Car;
+import model.cars.Cars;
 import model.movement.MovementStrategy;
-import model.vo.Car;
-import model.vo.Cars;
 import model.vo.Names;
 import model.vo.NumberOfAttempt;
 import org.junit.jupiter.api.DisplayName;
@@ -29,26 +29,6 @@ class RacingGameTest {
     }
 
     @Test
-    @DisplayName("입력받은 자동차 이름들과 시도 횟수를 기반으로 , RacingGame 객체를 만든다.")
-    void create() {
-        //given
-        Names names = new Names(List.of("apple", "hello", "hi"));
-        int numberOfAttempt = 5;
-        Cars expect = new Cars(List.of(
-                new Car("apple", 0),
-                new Car("hello", 0),
-                new Car("hi", 0))
-        );
-
-        //when
-        racingGame = new RacingGame(names, numberOfAttempt);
-
-        //then
-        assertThat(racingGame).extracting("racingCars")
-                .isEqualTo(expect);
-    }
-
-    @Test
     @DisplayName("생체 생성 시, 입력받은 값이 null 이면 예외처리를 반환한다.")
     void validateNull() {
         //given
@@ -60,20 +40,12 @@ class RacingGameTest {
     }
 
     @Test
-    @DisplayName("입력 받은 움직임 전략에 따라서 자동차를 이동 시킨다. 그리고 numberOfAttempt 를 하나 줄인다.")
+    @DisplayName("입력 받은 움직임 전략에 따라서 자동차를 이동 시킨 후, numberOfAttempt 를 하나 줄인다.")
     void playOneRound() {
         //given
-        Cars cars = new Cars(List.of(
-                new Car("apple", 0),
-                new Car("hello", 0),
-                new Car("hi", 0))
-        );
+        Names names = new Names(List.of("apple", "hello", "hi"));
+        Cars cars = new Cars(names);
         NumberOfAttempt numberOfAttempt = new NumberOfAttempt(1);
-        Cars expectCars = new Cars(List.of(
-                new Car("apple", 1),
-                new Car("hello", 1),
-                new Car("hi", 1))
-        );
         NumberOfAttempt expectNumberOfAttempt = new NumberOfAttempt(0);
         racingGame = new RacingGame(cars, numberOfAttempt);
 
@@ -81,8 +53,7 @@ class RacingGameTest {
         racingGame.playOneRound(testMovementStrategy);
 
         //then
-        assertThat(racingGame).extracting("racingCars", "numberOfAttempt")
-                .containsExactly(expectCars, expectNumberOfAttempt);
+        assertThat(racingGame).hasFieldOrPropertyWithValue("numberOfAttempt", expectNumberOfAttempt);
     }
 
     @ParameterizedTest
@@ -128,14 +99,13 @@ class RacingGameTest {
     @DisplayName("게임 내 자동차들의 포지션 리스트를 반환한다.")
     void getCarPositions() {
         //given
-        Cars cars = new Cars(List.of(
-                new Car("apple", 1),
-                new Car("apple", 2),
-                new Car("hi", 3))
-        );
+        Names names = new Names(List.of("apple", "hello", "hi"));
+        Cars cars = new Cars(names);
         NumberOfAttempt numberOfAttempt = new NumberOfAttempt(2);
         racingGame = new RacingGame(cars, numberOfAttempt);
-        List<Integer> expect = List.of(1, 2, 3);
+        racingGame.playOneRound(testMovementStrategy);
+        racingGame.playOneRound(testMovementStrategy);
+        List<Integer> expect = List.of(2, 2, 2);
 
         //when
         List<Integer> actual = racingGame.getCarPositionsDuringRacing();
@@ -148,6 +118,7 @@ class RacingGameTest {
     @DisplayName("우승자 이름으로 구성된 리스트를 반환한다.")
     void getWinnerNames() {
         //given
+
         Cars cars = new Cars(
                 List.of(new Car("hi", 2),
                         new Car("apple", 2),
